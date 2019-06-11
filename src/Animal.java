c12c6874e7a96f7b775bcf81fb4e14319f7796b7
Heymann <heymannsharma7161@gmail.com>
2019-06-10 23:07:09 -0400

    first change

---
+++ src/Animal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author heymannsharma
+ */
+public class Animal {
+    
+}
+public static void main(String args[])
+{
+Dog dog=new Dog("Lucifier");
+Cat cat=new Cat("Bellie");
+
+System.out.println()
+}
---
+++ src/animalexample/AnimalExample.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animalexample;
+
+/**
+ *
+ * @author heymannsharma
+ */
+public class AnimalExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    protected String name;
+
+    public Animal
+
+    public String play() {
+        return 
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
